1c0ce70a5863a77960c4e22c8a71a725dbe1f3de
nyiri7 <91242959+nyiri7@users.noreply.github.com>
2023-09-21 18:42:23 +0200

    a

--- 2023.09.13/sidenav.js
+++ 2023.09.13/sidenav.js
@@ -1,9 +1,28 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    var menu_texts = document.getElementsByClassName("menu_item_text");
+
+    for(var i = 0; i < menu_texts.length; i++){
+      menu_texts[i].style.visibility = "visible";
+    }
+
+    document.getElementById("mySidenav").style.width = "15vw";
+    document.getElementById("main_area").style.opacity ="0.3";
+    document.getElementById("closebtn").style.visibility = "visible";
+    document.getElementById("openbtn").style.visibility = "hidden";
+    document.getElementById("alma").style.visibility = "hidden";
+
+    
   }
 
   function closeNav() {
-    document.getElementById("mySidenav").style.width = "5vw";
-    document.getElementById("main").style.marginLeft = "5vw";
+    var menu_texts = document.getElementsByClassName("menu_item_text");
+    for(var i = 0; i < menu_texts.length; i++){
+      menu_texts[i].style.visibility = "hidden";
+    }
+
+    document.getElementById("mySidenav").style.width = "2vw";
+    document.getElementById("main_area").style.opacity = "1";
+    document.getElementById("closebtn").style.visibility = "hidden";
+    document.getElementById("openbtn").style.visibility = "visible";
+
   }
